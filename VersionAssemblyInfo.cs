3526af73e5fe5999458f55970e8750cd97d8a9ef
philo <phil@stormid.com>
2018-02-19 17:00:35 +0000

    removes stylecop analysers from nuget dependencies

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-ci.0+Branch.master.Sha.ff1e85802f5b2f6088e2d08c52ac24f34bc35d4b")]
+[assembly: AssemblyInformationalVersion("1.0.0-ci.0+Branch.master.Sha.32f37d94bf4d4065024555b690575f8494cdf45c")]
 
